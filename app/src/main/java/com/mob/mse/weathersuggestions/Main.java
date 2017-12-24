a62ab227c26184ee44fad3d891de3c44da5a7991
imedaouidene <imed.aouidene@gmail.com>
2017-12-24 18:16:00 +0100

    Splach screen --> DONE

--- app/src/main/java/com/mob/mse/weathersuggestions/Main.java
+++ app/src/main/java/com/mob/mse/weathersuggestions/Main.java
@@ -1,15 +1,13 @@
 package com.mob.mse.weathersuggestions;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -20,21 +18,10 @@ public class Main extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitymain);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer,null, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
@@ -73,7 +60,8 @@ public class Main extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
-
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -81,6 +69,9 @@ public class Main extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.home) {
+            current_weather fragment = new current_weather() ;
+            fragmentTransaction.add(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
             // Handle the camera action
         } else if (id == R.id.search) {
 
