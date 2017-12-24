ef676553ed9b196ecddfdcb4e1fd0eaa7e3542d1
imedaouidene <imed.aouidene@gmail.com>
2017-12-24 15:31:42 +0100

    Splach screen --> DONE

--- app/src/main/java/com/mob/mse/weathersuggestions/MainActivity.java
+++ app/src/main/java/com/mob/mse/weathersuggestions/MainActivity.java
@@ -1,13 +1,90 @@
 package com.mob.mse.weathersuggestions;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import static com.mob.mse.weathersuggestions.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
+        view= findViewById(android.R.id.content) ;
+        getSupportActionBar().hide(); // hiding Support Action Bar
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.White));
+
+        //Thread to pass automatically to next layout
+
+
+       timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(MainActivity.this,Main.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+
+        checkconnection() ;
+
+
+    }
+    Snackbar nointernet;
+    Thread timerThread ;
+    View view ;
+    void checkconnection() {
+
+
+        nointernet  = Snackbar.make(view,"No internet connection! ",Snackbar.LENGTH_INDEFINITE)
+                .setAction("RETRY", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        checkconnection() ;
+                    }
+                });
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        boolean  b = cm.getActiveNetworkInfo() != null;
+        if (b) {
+            try {
+                nointernet.dismiss();
+                timerThread.run();
+            } catch (Exception e){
+                Log.w("ERROR","ERROR in snack bar   "+e.toString() );
+
+            }
+        }else{
+            try {
+                nointernet.show();
+             }catch (Exception e){
+                Log.w("ERROR","ERROR in snack bar   "+e.toString() );
+
+            }
+        }
     }
 }
