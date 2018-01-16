9c4b3da3bb74255614c49a8642064e95fcaf5eff
imedaouidene <imed.aouidene@gmail.com>
2018-01-16 01:24:31 +0100

    improving favorits

--- app/src/main/java/com/mob/mse/weathersuggestions/fragments/favorits.java
+++ app/src/main/java/com/mob/mse/weathersuggestions/fragments/favorits.java
@@ -1,20 +1,37 @@
 package com.mob.mse.weathersuggestions.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.mob.mse.weathersuggestions.JSON.CountryinfoLoader;
+import com.mob.mse.weathersuggestions.JSON.ImageLoader;
 import com.mob.mse.weathersuggestions.R;
 import com.mob.mse.weathersuggestions.adapter.ItemCityAdapter;
+import com.mob.mse.weathersuggestions.adapter.SlidingImage_Adapter;
+import com.mob.mse.weathersuggestions.data.Utils;
+import com.mob.mse.weathersuggestions.model.Countryinfo;
+import com.mob.mse.weathersuggestions.model.ForecastResponse;
+import com.mob.mse.weathersuggestions.model.ImageResponse;
 import com.mob.mse.weathersuggestions.model.ItemCity;
+import com.mob.mse.weathersuggestions.model.ItemForecast;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -70,12 +87,10 @@ public class favorits extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-    SharedPreferences preferences ;
-    SharedPreferences.Editor edit;
+
     TextView nofav;
     ListView lv;
 
-    Gson gson ;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -87,7 +102,7 @@ preferences    = getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE
         gson= new Gson();
         nofav = (TextView)root.findViewById(R.id.nofav);
         lv = (ListView)root.findViewById(R.id.favorislist);
-        ArrayList<ItemCity> favorits = new ArrayList<>() ;
+        final ArrayList<ItemCity> favorits = new ArrayList<>() ;
 
         Set<String> cities = preferences.getStringSet("fav", new HashSet<String>());
 
@@ -106,21 +121,187 @@ preferences    = getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE
               nofav.setVisibility(View.INVISIBLE);
             }
 
-        ItemCityAdapter itemCityAdapter = new ItemCityAdapter(getContext(),android.R.layout.simple_list_item_1,favorits) ;
+        final ItemCityAdapter itemCityAdapter = new ItemCityAdapter(getContext(),android.R.layout.simple_list_item_1,favorits) ;
 
         lv.setAdapter(itemCityAdapter);
 
 
 
+       lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+               ItemCity itemCity = (ItemCity) adapterView.getItemAtPosition(i);
+               showinfos(getContext(),itemCity);
+
+           }
+       });
+final Context context= getContext() ;
+utils = new Utils(getContext()) ;
+
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(final AdapterView<?> adapterView, View view, int i, long l) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context) ;
+
+                final ItemCity itemCity = (ItemCity) adapterView.getItemAtPosition(i);
+            builder.setTitle("delete from favorits")
+                    .setMessage("Do you want to delete "+itemCity.getCity()+" from the list of favorits ?")
+                    .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            favorits.remove(itemCity);
+                            itemCityAdapter.notifyDataSetChanged();
+                            Set<String> cities = new HashSet<String>()  ;
+                            for (int j=0 ; j<favorits.size();j++){
+                                ItemCity k  =favorits.get(j);
+                                String thiscity = gson.toJson(k) ;
+                                cities.add(thiscity) ;
+
+                            }
 
 
+                            edit.clear();
+                            edit.putStringSet("fav",cities) ;
+                            edit.apply();
 
+                        }
+                    }).setNegativeButton("Ignore", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }) ;
+                AlertDialog diag ;
+                diag = builder.create();
+                diag.show();
+                return false;
+            }
+        });
 
 
 
         return root;
     }
 
+
+Utils utils ;
+    TextView country_name  ;
+    TextView ci_population ;
+    TextView ci_languages ;
+    TextView ci_currency ;
+    Button addtofavorits ;
+    ViewPager pager ;
+    LinearLayout linearLayout ;
+    SharedPreferences.Editor edit;
+    Gson gson = new Gson() ;
+    SharedPreferences preferences;
+
+    public void showinfos(final Context context , final ItemCity city){
+
+
+        Dialog dialog = new Dialog(context) ;
+        dialog.setContentView(R.layout.city_info);
+        ImageView flag = (ImageView)dialog.findViewById(R.id.city_flag) ;
+        TextView city_name =(TextView)dialog.findViewById(R.id.city_name) ;
+        country_name = (TextView)dialog.findViewById(R.id.ci_country_name) ;
+        ci_population = (TextView)dialog.findViewById(R.id.ci_population) ;
+        ci_languages = (TextView)dialog.findViewById(R.id.ci_languages) ;
+        ci_currency = (TextView)dialog.findViewById(R.id.ci_currency) ;
+        addtofavorits = (Button)dialog.findViewById(R.id.addtofavorits) ;
+        pager = (ViewPager)dialog.findViewById(R.id.pager) ;
+        linearLayout  =(LinearLayout)dialog.findViewById(R.id.listview) ;
+        addtofavorits.setVisibility(View.INVISIBLE);
+
+        //filling data
+        Picasso.with(getContext())
+                .load(Utils.getFlagURL(city.getItemLocation().getJsonWeather().sys.country.toLowerCase()))
+                .into(flag);
+
+        city_name.setText(city.getCity());
+
+        CountryinfoLoader.placeIdTask placeIdTask = new CountryinfoLoader.placeIdTask(new CountryinfoLoader.AsyncResponse(){
+
+            @Override
+            public void processFinish(Countryinfo countryinfo) {
+                country_name.setText(countryinfo.name);
+                ci_population.setText(Long.toString(countryinfo.population));
+                String language ="";
+                for (int i=0 ; i<countryinfo.languages.size();i++){
+                    language = language + countryinfo.languages.get(i).name+";";
+                }
+                // Toast.makeText(context,language,Toast.LENGTH_SHORT).show();
+                ci_languages.setText(language);
+                String currencies = "" ;
+                for(int i=0; i<countryinfo.currencies.size();i++){
+                    currencies = currencies + countryinfo.currencies.get(i).name+";";
+
+                }
+                // Toast.makeText(context,countryinfo.languages.get(0).,Toast.LENGTH_SHORT).show();
+                ArrayList<ItemForecast> forecasts = new ArrayList<ItemForecast>();
+
+                ForecastResponse forecastResponse = city.getItemLocation().getJsonForecast();
+                for (int i = 1; i < 7; i++) {
+                    ItemForecast fcs = new ItemForecast();
+                    fcs.setTemp(Integer.toString((int)(forecastResponse.list.get(i).temp.day+0.0f))+"°C");
+                    fcs.setDay(Utils.getDay(forecastResponse.list.get(i).dt));
+                    fcs.setDesc(forecastResponse.list.get(i).weather.get(0).main);
+                    fcs.setIcon(forecastResponse.list.get(i).weather.get(0).icon);
+                    forecasts.add(fcs);
+                }
+                setViewList1(forecasts,context,linearLayout);
+
+                ci_currency.setText(currencies) ;
+
+
+
+            }
+        },getContext());
+
+
+        placeIdTask.execute(city.getCountry());
+
+
+        ImageLoader.placeIdTask imageloader = new ImageLoader.placeIdTask(new ImageLoader.AsyncResponse() {
+            @Override
+            public void processFinish(ImageResponse imageResponse) {
+
+                for (int i = 0; i < imageResponse.hits.size(); i++)
+                    ImagesArray.add(imageResponse.hits.get(i).webformatURL+"?key=7593479-4b373fb7ca049dd32f5c81299");
+
+
+                pager.setAdapter(new SlidingImage_Adapter(getContext(), ImagesArray));
+
+
+                NUM_PAGES = ImagesArray.size()-1 ;
+
+
+
+
+
+            }
+        },context) ;
+        imageloader.execute(city.getCity());
+        dialog.show();
+
+
+    }
+    ArrayList<String> ImagesArray = new ArrayList<String>();
+    int NUM_PAGES ,currentPage;
+    public void setViewList1(ArrayList<ItemForecast> forecasts,Context context,LinearLayout linearLayout){
+        ImagesArray.clear();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        linearLayout.removeAllViews();
+        for (ItemForecast obje : forecasts) {
+            View view;
+            view = inflater.inflate(R.layout.weather_element, linearLayout, false);
+
+            ((TextView) view.findViewById(R.id.tv_f_temp)).setText(obje.getTemp());
+            ((TextView) view.findViewById(R.id.tv_f_day)).setText(obje.getDay());
+            ((TextView) view.findViewById(R.id.tv_f_desc)).setText(obje.getDesc());
+            ImageView img =(ImageView) view.findViewById(R.id.img_f_icon);
+            utils.setDrawableSmallIcon(obje.getIcon(), img);
+            linearLayout.addView(view);
+        }}
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
